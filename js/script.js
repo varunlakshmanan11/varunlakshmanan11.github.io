aea0b169d0943b806fff251298beb2de58e3209b
varunlakshmanan11 <varunl11@umd.edu>
2025-02-19 01:30:58 -0500

    modified:   css/styles.css 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -60,3 +60,12 @@ function toggleMenu() {
   const navbar = document.querySelector('.navbar');
   navbar.classList.toggle('active');
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.querySelector(".menu-icon");
+  const navbar = document.querySelector(".navbar ul");
+
+  menuIcon.addEventListener("click", function () {
+      navbar.classList.toggle("active");
+  });
+});
